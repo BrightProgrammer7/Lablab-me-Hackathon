c640bc881cfb720c2b16b0d0a2513896432f6972
BrightProgrammer7 <abdelilahk627@gmail.com>
2023-08-12 11:53:54 +0100

    third commit

---
+++ .history/gptech-news-backend/news/index_20230812112832.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+let newsURL = process.env.NEWS_URL;
+
+let app = express();
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  const response = await axios(newsURL);
+  console.log(response.data);
+  res.json(response.data);
+});
+
+app.listen("3002", () => {
+  console.log("App is listening at http://localhost:3002");
+});
